2ddb9ebf9670f7819d918b7d3721b612f1b53668
João Moreno <joao.moreno@microsoft.com>
2023-03-19 18:27:59 +0100

    cleaner log

--- build/azure-pipelines/distro/mixin-quality.ts
+++ build/azure-pipelines/distro/mixin-quality.ts
@@ -34,7 +34,7 @@ function main() {
 		throw new Error('Missing VSCODE_QUALITY, skipping mixin');
 	}
 
-	log('[mixin]', `Mixing in distro quality...`);
+	log(`Mixing in distro quality...`);
 
 	const basePath = `.build/distro/mixin/${quality}`;
 
@@ -48,23 +48,23 @@ function main() {
 			let builtInExtensions = oss.builtInExtensions;
 
 			if (Array.isArray(distro.builtInExtensions)) {
-				log('[mixin]', 'Overwriting built-in extensions:', distro.builtInExtensions.map(e => e.name));
+				log('Overwriting built-in extensions:', distro.builtInExtensions.map(e => e.name));
 
 				builtInExtensions = distro.builtInExtensions;
 			} else if (distro.builtInExtensions) {
 				const include = distro.builtInExtensions['include'] ?? [];
 				const exclude = distro.builtInExtensions['exclude'] ?? [];
 
-				log('[mixin]', 'OSS built-in extensions:', builtInExtensions.map(e => e.name));
-				log('[mixin]', 'Including built-in extensions:', include.map(e => e.name));
-				log('[mixin]', 'Excluding built-in extensions:', exclude);
+				log('OSS built-in extensions:', builtInExtensions.map(e => e.name));
+				log('Including built-in extensions:', include.map(e => e.name));
+				log('Excluding built-in extensions:', exclude);
 
 				builtInExtensions = builtInExtensions.filter(ext => !include.find(e => e.name === ext.name) && !exclude.find(name => name === ext.name));
 				builtInExtensions = [...builtInExtensions, ...include];
 
-				log('[mixin]', 'Final built-in extensions:', builtInExtensions.map(e => e.name));
+				log('Final built-in extensions:', builtInExtensions.map(e => e.name));
 			} else {
-				log('[mixin]', 'Inheriting OSS built-in extensions', builtInExtensions.map(e => e.name));
+				log('Inheriting OSS built-in extensions', builtInExtensions.map(e => e.name));
 			}
 
 			const result = { webBuiltInExtensions: oss.webBuiltInExtensions, ...distro, builtInExtensions };
@@ -73,7 +73,7 @@ function main() {
 			fs.cpSync(distroPath, ossPath, { force: true, recursive: true });
 		}
 
-		log('[mixin]', distroPath, '✔︎');
+		log(distroPath, '✔︎');
 	}
 }
 
